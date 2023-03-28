4c48c7efe5524cf15c8c917ce65a57ab823180c7
Алена Котова <mishka061@rambler.ru>
2023-03-28 18:01:55 +0300

    tests deploys

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,17 +1,14 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views.recipes import IngredientViewSet, RecipeViewSet, TagViewSet
 from api.views.users import UsersViewSet
 
-app_name = 'api'
-
 router = DefaultRouter()
-
-router.register('users', UsersViewSet)
-router.register('ingredients', IngredientViewSet)
-router.register('recipes', RecipeViewSet)
-router.register('tags', TagViewSet)
+router.register(r'users', UsersViewSet)
+router.register(r'ingredients', IngredientViewSet)
+router.register(r'recipes', RecipeViewSet)
+router.register(r'tags', TagViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
