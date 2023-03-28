062cc250b92a09482485fd1cd103e906184da890
Алена Котова <mishka061@rambler.ru>
2023-03-28 17:20:24 +0300

    tests deploys

--- backend/api/views.py
+++ backend/api/views.py
@@ -13,9 +13,6 @@ from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from .filters import IngredientFilter, RecipeFilter
 from .pagination import CustomPagination
 from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
-from .serializers import (IngredientSerializer, RecipeReadSerializer,
-                          RecipeShortSerializer, RecipeWriteSerializer,
-                          TagSerializer)
 
 
 class IngredientViewSet(ReadOnlyModelViewSet):
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,13 +27,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
     'corsheaders',
-
     'recipes.apps.RecipesConfig',
     'users.apps.UsersConfig',
     'api.apps.ApiConfig'
