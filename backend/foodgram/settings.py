a8f4243dc5d6571199b010cc8d0b2b33c5ae0772
Алена Котова <mishka061@rambler.ru>
2023-03-29 16:27:26 +0300

    tests deploys

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -141,11 +141,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/back_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'back_static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/back_media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'back_media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # djoser settings
 
