dd624e61d72865a4c6324453a2f10441bc15ac10
Алена Котова <mishka061@rambler.ru>
2023-03-30 07:04:35 +0300

    tests deploys

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
