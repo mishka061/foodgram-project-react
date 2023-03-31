f19afb2b5b4d387335348f8705fc58cdd94e0ad4
Алена Котова <mishka061@rambler.ru>
2023-03-31 14:24:32 +0300

    add

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -18,6 +18,9 @@ from api.serializers.users import UsersSerializer
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                             ShoppingCart, Tag)
 
+
+from users.models import Follow, User
+
 User = get_user_model()
 
 
@@ -406,3 +409,70 @@ class RecipeInfoSerializer(ModelSerializer):
     class Meta:
         model = Recipe
         fields = ('id', 'name', 'image', 'cooking_time')
+
+class UsersCreateSerializer(UserCreateSerializer):
+    """Сериализатор для обработки запросов на создание пользователя.
+    Валидирует создание пользователя с юзернеймом 'me'."""
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'password')
+
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise ValidationError(
+                'Невозможно создать пользователя с таким именем!'
+            )
+        return value
+
+
+class UsersSerializer(UserSerializer):
+    """Сериализатор для отображения информации о пользователе."""
+    is_subscribed = SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed'
+        )
+
+    def get_is_subscribed(self, object):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Follow.objects.filter(user=user, author=object.id).exists()
+
+
+class FollowSerializer(UsersSerializer):
+    """Сериализатор для добавления/удаления подписки, просмотра подписок."""
+    recipes = SerializerMethodField(read_only=True)
+    recipes_count = SerializerMethodField(read_only=True)
+
+    class Meta(UsersSerializer.Meta):
+        fields = UsersSerializer.Meta.fields + ('recipes', 'recipes_count')
+
+    def get_recipes(self, object):
+        from api.serializers.recipes import RecipeInfoSerializer
+
+        request = self.context.get('request')
+        context = {'request': request}
+        recipe_limit = request.query_params.get('recipe_limit')
+        queryset = object.recipes.all()
+        if recipe_limit:
+            queryset = queryset[:int(recipe_limit)]
+        return RecipeInfoSerializer(queryset, context=context, many=True).data
+
+    def get_recipes_count(self, object):
+        return object.recipes.count()
