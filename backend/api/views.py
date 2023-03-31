3df067d0b954877728d42d640a913a39ef9c7ba1
Алена Котова <mishka061@rambler.ru>
2023-03-31 15:00:37 +0300

    add

--- backend/api/views.py
+++ backend/api/views.py
@@ -37,25 +37,87 @@ from users.models import Follow, User
 
 
 class IngredientViewSet(ReadOnlyModelViewSet):
+    """Вьюсет для обработки запросов на получение ингредиентов."""
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (AllowAny, IsAdminOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
     filterset_class = IngredientFilter
 
 
 class TagViewSet(ReadOnlyModelViewSet):
+    """Вьюсет для обработки запросов на получение тегов."""
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (AllowAny, IsAdminOrReadOnly,)
 
 
 class RecipeViewSet(ModelViewSet):
+    """Вьюсет для работы с рецептами.
+     Обработка запросов создания/получения/редактирования/удаления рецептов
+     Добавление/удаление рецепта в избранное и список покупок"""
     queryset = Recipe.objects.all()
     permission_classes = (IsAuthorOrReadOnly | IsAdminOrReadOnly,)
     pagination_class = CustomPagination
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RecipeFilter
+    serializer_class = RecipeSerializer
+
+    def action_post_delete(self, pk, serializer_class):
+        user = self.request.user
+        recipe = get_object_or_404(Recipe, pk=pk)
+        object = serializer_class.Meta.model.objects.filter(
+            user=user, recipe=recipe
+        )
+
+        if self.request.method == 'POST':
+            serializer = serializer_class(
+                data={'user': user.id, 'recipe': pk},
+                context={'request': self.request}
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if self.request.method == 'DELETE':
+            if object.exists():
+                object.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response({'error': 'Этого рецепта нет в списке'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    @action(methods=['POST', 'DELETE'], detail=True)
+    def favorite(self, request, pk):
+        return self.action_post_delete(pk, FavoriteSerializer)
+
+    @action(methods=['POST', 'DELETE'], detail=True)
+    def shopping_cart(self, request, pk):
+        return self.action_post_delete(pk, ShoppingCartSerializer)
+
+    @action(
+        detail=False,
+        methods=['GET'],
+        url_path='download_shopping_cart',
+        url_name='download_shopping_cart',
+        pagination_class=None,
+        permission_classes=[IsAuthorOrReadOnly]
+    )
+    def download_basket(self, request):
+        ingredients = RecipeIngredient.objects.filter(
+            recipe__shopping_cart__user=request.user
+        ).values(
+            'ingredient__name',
+            'ingredient__measurement_unit'
+        ).order_by('ingredient__name').annotate(total=Sum('amount'))
+        result = 'Cписок покупок:\n\nНазвание продукта - Кол-во/Ед.изм.\n'
+        for ingredient in ingredients:
+            result += ''.join([
+                f'{ingredient["ingredient__name"]} - {ingredient["total"]}/'
+                f'{ingredient["ingredient__measurement_unit"]} \n'
+            ])
+        response = HttpResponse(result, content_type='text/plain')
+        response['Content-Disposition'] = f'attachment; filename={FILENAME}'
+        return response
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -137,91 +199,6 @@ class RecipeViewSet(ModelViewSet):
         response = HttpResponse(shopping_list, content_type='text/plain')
         response['Content-Disposition'] = f'attachment; filename={filename}'
 
-        return response
-
-class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
-    """Вьюсет для обработки запросов на получение ингредиентов."""
-    queryset = Ingredient.objects.all()
-    serializer_class = IngredientSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filterset_class = IngredientFilter
-    permission_classes = (AllowAny,)
-    pagination_class = None
-
-
-class TagViewSet(viewsets.ReadOnlyModelViewSet):
-    """Вьюсет для обработки запросов на получение тегов."""
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
-    permission_classes = (AllowAny,)
-    pagination_class = None
-
-
-class RecipeViewSet(viewsets.ModelViewSet):
-    """Вьюсет для работы с рецептами.
-     Обработка запросов создания/получения/редактирования/удаления рецептов
-     Добавление/удаление рецепта в избранное и список покупок"""
-    queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filterset_class = RecipeFilter
-    permission_classes = (IsAuthorOrReadOnly,)
-    pagination_class = LimitPagination
-
-    def action_post_delete(self, pk, serializer_class):
-        user = self.request.user
-        recipe = get_object_or_404(Recipe, pk=pk)
-        object = serializer_class.Meta.model.objects.filter(
-            user=user, recipe=recipe
-        )
-
-        if self.request.method == 'POST':
-            serializer = serializer_class(
-                data={'user': user.id, 'recipe': pk},
-                context={'request': self.request}
-            )
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        if self.request.method == 'DELETE':
-            if object.exists():
-                object.delete()
-                return Response(status=status.HTTP_204_NO_CONTENT)
-            return Response({'error': 'Этого рецепта нет в списке'},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-    @action(methods=['POST', 'DELETE'], detail=True)
-    def favorite(self, request, pk):
-        return self.action_post_delete(pk, FavoriteSerializer)
-
-    @action(methods=['POST', 'DELETE'], detail=True)
-    def shopping_cart(self, request, pk):
-        return self.action_post_delete(pk, ShoppingCartSerializer)
-
-    @action(
-        detail=False,
-        methods=['GET'],
-        url_path='download_shopping_cart',
-        url_name='download_shopping_cart',
-        pagination_class=None,
-        permission_classes=[IsAuthorOrReadOnly]
-    )
-    def download_basket(self, request):
-        ingredients = RecipeIngredient.objects.filter(
-            recipe__shopping_cart__user=request.user
-        ).values(
-            'ingredient__name',
-            'ingredient__measurement_unit'
-        ).order_by('ingredient__name').annotate(total=Sum('amount'))
-        result = 'Cписок покупок:\n\nНазвание продукта - Кол-во/Ед.изм.\n'
-        for ingredient in ingredients:
-            result += ''.join([
-                f'{ingredient["ingredient__name"]} - {ingredient["total"]}/'
-                f'{ingredient["ingredient__measurement_unit"]} \n'
-            ])
-        response = HttpResponse(result, content_type='text/plain')
-        response['Content-Disposition'] = f'attachment; filename={FILENAME}'
         return response
 
 
