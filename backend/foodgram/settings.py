11a72ef88ea89d246819852cbdb33486b980d0b4
Алена Котова <mishka061@rambler.ru>
2023-04-02 11:24:18 +0300

    add

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -92,8 +92,8 @@ else:
             'NAME': os.getenv('DB_NAME', 'postgres'),
             'USER': os.getenv('POSTGRES_USER', 'postgres'),
             'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-            'HOST': os.getenv('DB_HOST', 'db'),
-            'PORT': os.getenv('DB_PORT', '5432')
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT')
         }
     }
 
