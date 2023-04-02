90247b1591ddf00149f143673c247f6341fa759e
Алена Котова <mishka061@rambler.ru>
2023-04-02 20:20:44 +0300

    add

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 12345
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['51.250.101.96', 'localhost', '127.0.0.1', 'backend']
 
